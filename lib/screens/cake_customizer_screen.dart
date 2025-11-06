8b6d29c62bfec38b361d9d4780554fb27f0176c4
mathewsa <mathewastorga321@gmail.com>
2025-11-06 14:28:53 +0800

    Update workflow

--- lib/screens/cake_customizer_screen.dart
+++ lib/screens/cake_customizer_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 class CakeCustomizerScreen extends StatefulWidget {
@@ -13,6 +14,9 @@ class CakeCustomizerScreen extends StatefulWidget {
 class _CakeCustomizerScreenState extends State<CakeCustomizerScreen> {
   @override
   Widget build(BuildContext context) {
+    final String modelPath = kIsWeb
+        ? 'assets/assets/cake_layers/${widget.cakeShape}_base.glb'
+        : 'assets/cake_layers/${widget.cakeShape}_base.glb';
     return Scaffold(
       appBar: AppBar(
         title: const Text('Customize Your Cake'),
@@ -20,7 +24,9 @@ class _CakeCustomizerScreenState extends State<CakeCustomizerScreen> {
       ),
       body: ModelViewer(
         backgroundColor: Colors.white,
-        src: 'assets/cake_layers/${widget.cakeShape}_base.glb',
+        // Flutter Web serves assets under build/web/assets/assets/**
+        // so we use a double 'assets' path on web builds.
+        src: modelPath,
         alt: 'A 3D model of a cake',
         ar: true,
         autoRotate: true,
