40fd69109b3f9284946fcf935a78a9195f4f2ae4
mathewsa <mathewastorga321@gmail.com>
2025-11-07 22:57:18 +0800

    v3.1.3

--- build_and_rename.bat
+++ build_and_rename.bat
@@ -10,10 +10,11 @@ copy build\app\outputs\flutter-ios\ipa\Runner.ipa build\app\outputs\flutter-ios\
 echo IPA built and renamed to: build\app\outputs\flutter-ios\inbento-kiosk-v1.0.0.ipa
 pause
 
-git add pubspec.yaml
-git commit -m "Bump version to 3.1.0"
-git tag v3.1.0
-git push origin v3.1.0
+git add .
+git commit -m "Bump version to %VERSION%"
+git push origin main
+git tag v%VERSION%
+git push origin v%VERSION%
 
 
 git add .
