1925c4abf7b0198964a58e35525173b01ab3ccc7
Ndkorr <Ndkorr@users.noreply.github.com>
2025-11-09 17:39:10 +0000

    deploy: 6384787b3a6f6952f7575ccd5cda92de6ec9bb16

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3034874108"
+    serviceWorkerVersion: "2417830150"
   }
 });
